22b3ba67664abceedc5bffc51f077d3a3be2d8d0
Joris Sparla <joris.sparla@gmail.com>
2019-05-19 09:59:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Amplify from "aws-amplify";
+import { withAuthenticator, AmplifyTheme } from "aws-amplify-react"; // or 'aws-amplify-react-native';
+import awsexports from "./aws-exports";
 import "./App.css";
 
+Amplify.configure(awsexports);
 class App extends React.Component {
   state = {};
 
@@ -9,4 +13,21 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme,
+    background: "linear-gradient(to right, rgb(82, 71, 99) 0%, rgb(55, 49, 66) 100%)",
+    color: "white"
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: "var(--light-blue)",
+    color: "white"
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "var(--light-blue)"
+  }
+};
+export default withAuthenticator(App, true, [], null, theme);
